5660d8625221b093dd4b57e9d3bf15047d35031e
lotosv2010 <lotosv2010@163.com>
2019-10-11 18:34:20 +0800

    mobx demo

---
+++ example/03-singlePage/02-react/mobx-demo/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MobxDemo from './mobx/index'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <MobxDemo />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ example/03-singlePage/02-react/mobx-demo/src/store/index.js
@@ -0,0 +1,7 @@
+import friend from './friend'
+import post from './post'
+
+export default {
+  friend,
+  post
+}
