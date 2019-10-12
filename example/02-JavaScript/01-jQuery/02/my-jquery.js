4e0026a1c43b262a8a49e2208d7cd31d5a0327b9
lotosv2010 <lotosv2010@163.com>
2019-10-12 17:04:33 +0800

    选择器

--- example/02-JavaScript/01-jQuery/02/my-jquery.js
+++ example/02-JavaScript/01-jQuery/02/my-jquery.js
@@ -1,15 +1,52 @@
 (function(root){
-  var jQuery = function() {
-    return new jQuery.prototype.init()
+
+  var testExp = /^\s*(<[\w\W]+>)[^>]*$/;
+	var rejectExp = /^<(\w+)\s*\/?>(?:<\/\1>|)$/;
+  var version = '1.0.1'
+  var jQuery = function(selector, context) {
+    return new jQuery.prototype.init(selector, context)
   }
 
   // jQuery.prototype.init = function() {}
   jQuery.fn = jQuery.prototype = {
-    init: function() {
+    length: 0,
+		jquery: version,
+		selector: "",
+		init: function(selector, context) {
+			context = context || document;
+			var match, elem, index = 0;
+			//$()  $(undefined)  $(null) $(false)  
+			if (!selector) {
+				return this;
+			}
 
-    },
+			if (typeof selector === "string") {
+				if (selector.charAt(0) === "<" && selector.charAt(selector.length - 1) === ">" && selector.length >= 3) {
+					match = [selector]
+				}
+				//创建DOM
+				if (match) {
+					//this  
+					jQuery.merge(this, jQuery.parseHTML(selector, context)); 
+                //查询DOM节点
+				} else {
+					elem = document.querySelectorAll(selector);
+					var elems = Array.prototype.slice.call(elem);
+					this.length = elems.length;
+					for (; index < elems.length; index++) {
+						this[index] = elems[index];
+					}
+					this.context = context;
+					this.selector = selector;
+				}
+			} else if (selector.nodeType) {
+				this.context = this[0] = selector;
+				this.length = 1;
+				return this;
+			}
+		},
     css: function() {
-
+      console.log('this is css function')
     }
   }
 
@@ -72,7 +109,46 @@
     },
     isArray: function(obj) {
       return Array.isArray(obj)
-    }
+    },
+    isFunction: function(fn) {
+      return toString.call(fn) === '[object Function]'
+    },
+    markArray: function(arr, results) {
+      var ret = results || []
+      if(arr != null) {
+        jQuery.merge(ret, typeof arr === 'string' ? [arr] : arr)
+      }
+      return ret
+    },
+    // 合并数组
+    merge: function(first, second) {
+			var l = second.length,
+				i = first.length,
+				j = 0;
+
+			if (typeof l === "number") {
+				for (; j < l; j++) {
+					first[i++] = second[j];
+				}
+			} else {
+				while (second[j] !== undefined) {
+					first[i++] = second[j++];
+				}
+			}
+
+			first.length = i;
+
+			return first;
+		},
+		parseHTML: function(data, context) {
+			if (!data || typeof data !== "string") {
+				return null;
+			}
+			//过滤掉<a>   <a>   => a 
+			var parse = rejectExp.exec(data);
+			console.log(parse)
+			return [context.createElement(parse[1])];   
+		}
   })
 
   root.$ = root.jQuery = jQuery
