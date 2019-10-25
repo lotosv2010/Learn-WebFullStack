c18cd6c7287e4895a668e7fae961883115d71813
lotosv2010 <lotosv2010@163.com>
2019-10-25 17:57:52 +0800

    更新blog

---
+++ example/03-singlePage/04-vuepress/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+
+# 构建
+cnpm run build:build
+
+# 进入生成的构建文件夹
+cd blog/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:lotosv2010/blog.git master:gh-pages
+
+# 链接远程仓库
+git remote add origin https://github.com/lotosv2010/blog.git
+
+# 拉取远程仓库的文件
+git pull --rebase origin master
+
+# 同步更新代码
+git push -u origin master
+
+cd -
